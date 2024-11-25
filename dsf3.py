1a776a798dac0f300d71ad7198a8be3fbc0ec801
김준수 <72565743+chocoboy08@users.noreply.github.com>
2024-11-26 03:43:30 +0900

    3

--- dsf3.py
+++ dsf3.py
@@ -4,12 +4,10 @@ import geopandas as gpd
 import folium
 from streamlit_folium import st_folium
 
-# 1. Streamlit 클라우드 경로 설정
-EXCEL_FILE = "출산율통계최종.xlsx"
-SHP_FILE = "N3A_G0100000.shp"
+
 
 # 2. 엑셀 데이터 로드 및 전처리
-df_seoul_pop = pd.read_excel(EXCEL_FILE, engine='openpyxl', header=[0, 1])
+df_seoul_pop = pd.read_excel("출산율통계최종.xlsx", engine='openpyxl', header=[0, 1])
 df_seoul_pop.columns = ['_'.join(col).strip() for col in df_seoul_pop.columns.values]
 df_seoul_pop = df_seoul_pop[['행정구역별_행정구역별', '2023_합계출산율 (가임여성 1명당 명)']]
 
@@ -24,8 +22,9 @@ df_seoul_pop = df_seoul_pop[df_seoul_pop['행정구역별_행정구역별'] != '
 df_seoul_pop = df_seoul_pop.reset_index(drop=True)
 df_seoul_pop['행정구역별_행정구역별'] = df_seoul_pop['행정구역별_행정구역별'].str.strip()
 
-# 3. GeoJSON 데이터 로드 및 전처리
-gdf = gpd.read_file(SHP_FILE)
+# 3. shp 데이터 로드 및 전처리
+
+gdf = gpd.read_file("N3A_G0100000.fgb")
 gdf.rename(columns={'NAME': '행정구역별_행정구역별'}, inplace=True)
 
 # 중복된 행정구역 처리 함수
